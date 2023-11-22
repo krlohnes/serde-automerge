3154a1d6f2757dc7f7b319c11c02030cb52594e3
Marijn Suijten <marijn@traverseresearch.nl>
2023-11-22 13:26:40 +0100

    tests/updates: Test both `AutoCommit` and `Automerge` with manual `Transaction`

--- tests/updates.rs
+++ tests/updates.rs
@@ -28,7 +28,87 @@ const CAMERA: &str = "camera";
 const NUMBERS: &str = "numbers";
 
 #[test]
-fn test_updates_and_merging() {
+fn test_updates_and_merging_on_manual_commit() {
+    // Initial dummy values
+    let mut player_send = Player {
+        position: Position { x: 1, y: 2, z: 3 },
+        direction: Direction {
+            x: 10,
+            y: 11,
+            z: 12,
+        },
+    };
+    let mut camera_send = Camera {
+        x: 20,
+        y: 21,
+        z: 22,
+    };
+    let mut numbers_send = vec![31, 32, 33];
+
+    // Sending
+    let mut doc_send = Automerge::new();
+    let mut tx = doc_send.transaction();
+    let (player_id, camera_id, numbers_id) =
+        send(&mut tx, &player_send, &camera_send, &numbers_send);
+    let (commit, _patch_log) = tx.commit();
+    let base_commit = commit.unwrap();
+    // Send the whole document
+    let data = doc_send.save();
+
+    // Receiving
+    let mut doc_receive = Automerge::load(&data).unwrap();
+    let (mut player_receive, mut camera_receive, mut numbers_receive) = receive(&doc_receive);
+    assert_eq!(player_send, player_receive);
+    assert_eq!(camera_send, camera_receive);
+    assert_eq!(numbers_send, numbers_receive);
+
+    // Updating
+    let mut tx = doc_send.transaction();
+    update1(
+        &mut tx,
+        &mut player_send,
+        &mut camera_send,
+        &mut numbers_send,
+        &player_id,
+        &camera_id,
+        &numbers_id,
+    );
+    let (_change_hash_after_update1, _patch_log) =
+        tx.commit_with(CommitOptions::default().with_message("Updating position"));
+
+    assert_ne!(player_send, player_receive);
+
+    let data = doc_send.save_after(&[base_commit]);
+
+    // Update receiving side in the meantime too
+    let (player_id, camera_id, numbers_id) = (
+        doc_receive.get(ObjId::Root, PLAYER).unwrap().unwrap().1,
+        doc_receive.get(ObjId::Root, CAMERA).unwrap().unwrap().1,
+        doc_receive.get(ObjId::Root, NUMBERS).unwrap().unwrap().1,
+    );
+    update2(
+        &mut doc_receive,
+        &mut player_receive,
+        &mut camera_receive,
+        &mut numbers_receive,
+        &player_id,
+        &camera_id,
+        &numbers_id,
+    );
+
+    // Receiving
+    doc_receive.load_incremental(&data).unwrap();
+    let (player_receive2, camera_receive2, numbers_receive2) = receive(&doc_receive);
+
+    // Assert that local and remote changes were merged successfully
+    assert_eq!(player_send, player_receive2);
+
+    assert_eq!(camera_receive, camera_receive2);
+    assert_eq!(numbers_receive, numbers_receive2);
+}
+
+#[test]
+fn test_updates_and_merging_on_autocommit() {
     // Initial dummy values
     let mut player_send = Player {
         position: Position { x: 1, y: 2, z: 3 },
@@ -68,6 +148,7 @@ fn test_updates_and_merging() {
         &camera_id,
         &numbers_id,
     );
+    doc_send.commit_with(CommitOptions::default().with_message("Updating position"));
 
     assert_ne!(player_send, player_receive);
 
@@ -88,14 +169,12 @@ fn test_updates_and_merging() {
         &camera_id,
         &numbers_id,
     );
-    println!();
 
     // Receiving
     doc_receive.load_incremental(&data).unwrap();
     let (player_receive2, camera_receive2, numbers_receive2) = receive(&doc_receive);
 
     // Assert that local and remote changes were merged successfully
-    // XXX: Inline the functions, the code below implies knowing thwat update1() and update2() touched...
     assert_eq!(player_send, player_receive2);
 
     assert_eq!(camera_receive, camera_receive2);
@@ -126,7 +205,7 @@ fn send(
 }
 
 fn update1(
-    doc: &mut AutoCommit,
+    tx: &mut impl Transactable,
     player: &mut Player,
     camera: &mut Camera,
     numbers: &mut [i32],
@@ -141,12 +220,10 @@ fn update1(
 
     // We update _only_ the player with the previously obtained player_id
     // Since Serializer uses a &mut we can also use the transact functionallity
-    let (doc, _ex_id) = player
+    let (_doc, _ex_id) = player
         .position
-        .serialize(Serializer::new(doc, id_player.clone(), POSITION))
+        .serialize(Serializer::new(tx, id_player.clone(), POSITION))
         .expect("Serialize failed");
-
-    doc.commit_with(CommitOptions::default().with_message("Updating position"));
 
     println!("Update 1:");
     println!("{:?}", player);
